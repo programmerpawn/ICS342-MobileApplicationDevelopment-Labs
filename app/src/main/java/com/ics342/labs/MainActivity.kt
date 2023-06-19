d72a7c7afd88dd184f2431f7b209ec6044af4bd6
Pong Lee <pongeslee@gmail.com>
2023-06-19 17:14:26 -0500

    lab4

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -10,10 +10,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,7 +79,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun DataItemView(dataItem: DataItem) {
-    /* Create the view for the data item her. */
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
 
     //Creates a space for "Hello Android!".
     Spacer(modifier = Modifier.size(20.dp))
@@ -100,7 +108,16 @@ fun DataItemView(dataItem: DataItem) {
         }
 
     }
-
+    if (showDialog){
+        AlertDialog(
+            onDismissRequest = { showDialog = false},
+            title = { Text(text = dataItem.name)},
+            text = { Text(text = dataItem.description)},
+            confirmButton = { TextButton(onClick = { showDialog = false }) {
+                Text(text = "Okay")
+            }},
+        )
+    }
 }
 
 @Composable
@@ -114,6 +131,7 @@ fun DataItemList(dataItems: List<DataItem>) {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
