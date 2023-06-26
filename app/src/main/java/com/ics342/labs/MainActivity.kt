7e02a3058ad3aa3035226af904f1c0c4defd6c56
Pong Lee <pongeslee@gmail.com>
2023-06-26 12:04:05 -0500

    Assignment1

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,54 +3,33 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 
-
-private val dataItems = listOf(
-    DataItem(1, "Item 1", "Description 1"),
-    DataItem(2, "Item 2", "Description 2"),
-    DataItem(3, "Item 3", "Description 3"),
-    DataItem(4, "Item 4", "Description 4"),
-    DataItem(5, "Item 5", "Description 5"),
-    DataItem(6, "Item 6", "Description 6"),
-    DataItem(7, "Item 7", "Description 7"),
-    DataItem(8, "Item 8", "Description 8"),
-    DataItem(9, "Item 9", "Description 9"),
-    DataItem(10, "Item 10", "Description 10"),
-    DataItem(11, "Item 11", "Description 11"),
-    DataItem(12, "Item 12", "Description 12"),
-    DataItem(13, "Item 13", "Description 13"),
-    DataItem(14, "Item 14", "Description 14"),
-    DataItem(15, "Item 15", "Description 15"),
-    DataItem(16, "Item 16", "Description 16"),
-    DataItem(17, "Item 17", "Description 17"),
-    DataItem(18, "Item 18", "Description 18"),
-    DataItem(19, "Item 19", "Description 19"),
-    DataItem(20, "Item 20", "Description 20"),
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WeatherView()
                 }
             }
         }
@@ -78,66 +57,80 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DataItemView(dataItem: DataItem) {
-    var showDialog by remember {
-        mutableStateOf(false)
-    }
+fun WeatherView() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .padding(10.dp)
 
-    //Creates a space for "Hello Android!".
-    Spacer(modifier = Modifier.size(20.dp))
-
-    Row {
-        //Gets the dataItem ID and converts it to a string
-        Text(text = dataItem.id.toString())
-        Spacer(modifier = Modifier.size(15.dp))
-
-        Column {
-
-            //Gets the dataItem name
-            Text(
-                text = dataItem.name,
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.bodySmall,
-            )
-            //Gets the dataItem description
-            Text(
-                text = dataItem.description,
-                style = MaterialTheme.typography.bodySmall,
-                fontSize = 3.sp,
-                )
-        }
-
-    }
-    if (showDialog){
-        AlertDialog(
-            onDismissRequest = { showDialog = false},
-            title = { Text(text = dataItem.name)},
-            text = { Text(text = dataItem.description)},
-            confirmButton = { TextButton(onClick = { showDialog = false }) {
-                Text(text = "Okay")
-            }},
         )
-    }
-}
+        //Sets the location where the temperature is located
+        Text(
+            text = stringResource(id = R.string.myLocation),
+            fontSize = 15.sp,
+            modifier = Modifier.padding(10.dp)
+        )
 
-@Composable
-fun DataItemList(dataItems: List<DataItem>) {
-    /* Create the list here. This function will call DataItemView() */
 
-    //Creates a function for a column for dataItems.
-    LazyColumn {
-        items(dataItems) { DataItem ->
-            DataItemView(DataItem)
+        Row() {
+            Column(modifier = Modifier.padding(5.dp)) {
+                //Sets the current Temp
+                Text(
+                    text = stringResource(id = R.string.currentTempData),
+                    fontSize = 45.sp
+                )
+                //Sets the "feels like" temp
+                Text(
+                    text = stringResource(id = R.string.feelsLike),
+                    fontSize = 15.sp
+                )
+                Spacer(modifier = Modifier.height(25.dp))
+
+                //other information about the weather
+                Text(
+                    text = stringResource(id = R.string.dailyLow)
+                )
+                Text(
+                    text = stringResource(id = R.string.dailyHigh)
+                )
+                Text(
+                    text = stringResource(id = R.string.humidity)
+                )
+                Text(
+                    text = stringResource(id = R.string.stringPressure)
+                )
+
+            }
+
+            Column(modifier = Modifier.padding(5.dp)) {
+                //Sets the picture
+                val imageMod = Modifier
+                    .size(75.dp)
+                Image(
+                    painter = painterResource(id = R.drawable.sunny),
+                    contentDescription = "sun",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = imageMod
+
+                )
+            }
+
         }
     }
-}
 
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LabsTheme {
-        Greeting("Android")
-        DataItemList(dataItems = dataItems)
+        WeatherView()
     }
 }
